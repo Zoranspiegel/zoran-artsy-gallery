367342982bf59137710de64dc6c3ffd0c90343d7
Juan José Rodriguez <zoranbow@gmail.com>
2024-11-10 01:35:57 -0500

    env fix

--- src/lib/dbConnection.ts
+++ src/lib/dbConnection.ts
@@ -1,14 +1,19 @@
-import env from '@/env';
 import mongoose from 'mongoose';
 
 const connection: { isConnected?: number } = {};
 
 async function dbConnect(): Promise<void> {
-  if (connection.isConnected) return;
+  try {
+    if (connection.isConnected) return;
 
-  const db = await mongoose.connect(env.MONGODB_URI);
+    if (!process.env.MONGODB_URI) throw new Error('Internal Server Error');
 
-  connection.isConnected = db.connections[0].readyState;
+    const db = await mongoose.connect(process.env.MONGODB_URI);
+
+    connection.isConnected = db.connections[0].readyState;
+  } catch (error) {
+    throw error;
+  }
 }
 
 export default dbConnect;
